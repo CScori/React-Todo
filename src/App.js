0d86dc84c4423958747271ebbc8e35b1410c4c25
CScori <amilanotte1366@gmail.com>
2019-09-30 14:45:30 -0500

    adds toggle for completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Todo } from './components/TodoComponents/Todo'
+import TodoList from './components/TodoComponents/TodoList'
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todo: Todo
+    };
+  }
+
+  toggleItem = id => {
+    console.log(id);
+    this.setState({
+      todo: this.state.todo.map(aciton => {
+        if (action.id === id) {
+          return {
+            ...action,
+            completed: !action.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div>
+        
+        </div>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react'
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+
+
+const TodoList = () => {
+    return (
+       <div></div>
+            )
+        };
+        
+export default TodoList
+        
